605ea89e9fff19514227ba3f5ee3265ef1291045
kalifa <906326315@qq.com>
2018-06-14 10:12:12 +0800

    rebuild by node.js

---
+++ routes/index.js
@@ -0,0 +1,18 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Express' });
+});
+router.get('/login', function(req, res, next) {
+  res.render('login', {});
+});
+router.get('/signup', function(req, res, next) {
+  res.render('signup', {});
+});
+router.get('/index', function(req, res, next) {
+  res.render('index', {title:'Express'});
+});
+
+module.exports = router;
